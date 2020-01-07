e4bf137407deed749a919c33253fd54aa51d0848
svetaelar <svetaelar@gmail.com>
2020-01-07 21:25:47 +0300

    Какие то изменения

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,13 @@
 var navMain = document.querySelector (".main-nav");
 var navToggle = document.querySelector (".main-nav__toggle");
+var form = document.querySelector(".form");
+var popupSuccess = document.querySelector(".popup--success");
+var btnSuccess = popupSuccess.querySelector(".button--submit");
+var popupError = document.querySelector(".popup--error");
+var btnError = popupError.querySelector(".button--submit");
+var firstName = form.querySelector("[name=firstname]");
+var surname = form.querySelector("[name=surname]");
+var email = form.querySelector("[name=email]");
 
 navMain.classList.remove ("main-nav--nojs");
 navToggle.addEventListener ("click", function() {
@@ -12,3 +20,29 @@ navToggle.addEventListener ("click", function() {
     navMain.classList.remove ("main-nav--opened");
   }
   });
+
+form.addEventListener("submit", function(evt) {
+  evt.preventDefault();
+  console.log(surname);
+
+  if (!firstName.value || !surname.value || !email.value) {
+  popupError.classList.add("popup__show");
+  console.log(surname.value);
+  console.log(firstName.value);
+  console.log(name1);
+  console.log(email);
+  console.log(email.value);
+  } else {
+    popupSuccess.classList.add("popup__show");
+  }
+});
+
+btnSuccess.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupSuccess.classList.remove("popup__show");
+});
+
+btnError.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupError.classList.remove("popup__show");
+});
