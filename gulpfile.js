d2a79c3d84aab3126617c872b44e0155fdd6970a
svetaelar <svetaelar@gmail.com>
2020-01-15 19:16:45 +0300

    B еще немного отшлифовала

--- gulpfile.js
+++ gulpfile.js
@@ -109,5 +109,5 @@ gulp.task("clean", function () {
   return del("build");
 })
 
-gulp.task("build", gulp.series("clean", "js", "copy", "css", "webp", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "js", "copy", "css", "images", "webp", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
